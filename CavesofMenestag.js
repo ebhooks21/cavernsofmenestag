475a1669530c630b681cf2c96eaf2ced03efe230
Eric Hooks <ebhooks21@gmail.com>
2024-07-08 23:21:59 -0500

    Initial Commit.

---
+++ CavesofMenestag.js
@@ -0,0 +1,15 @@
+/**
+ * File: CavesofMenestag.js -- Main file for the Caves of Menestag game.
+ */
+const {app} = require('electron');
+
+let filePath = app.getAppPath();
+
+/**
+ * Main function for the game.
+ */
+function main() {
+
+}
+
+main();
